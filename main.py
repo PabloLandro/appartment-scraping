9185226938aae08574a912687ee27f067d9b98dc
PabloLandro <73550306+PabloLandro@users.noreply.github.com>
2024-03-30 19:55:12 +0100

    cleaner script

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import time
 import re
 import json
 
+from urllib.parse import urljoin
+
+from progress_bar import Progress_bar
+
 import csv
 from selenium import webdriver
 
@@ -20,10 +24,10 @@ from selenium.webdriver.chrome.options import Options
 URL = "https://www.homegate.ch/en"
 LOCATION = "Lugano"
 RADIUS = 10000
-MAX_PRICE = 2000
+MAX_PRICE = 1000
 ROOMS = 3.5
 
-CSV = True
+CSV = False
 OUTPUT_FILE = 'apartments.csv'
 
 BAR_WIDTH = 40
@@ -80,13 +84,54 @@ def search():
     search_button = driver.find_element(by=By.CSS_SELECTOR, value="button[data-cy=SearchBar_button]")
     search_button.click()
 
+
+
+def try_get_element_text(parent, selector):
+    out = None
+    try:
+        out = parent.find_element(By.CSS_SELECTOR, selector).text
+    except:
+        pass
+    return out
+
 def scrape_apartment (apartment_element):
+    name = try_get_element_text(apartment_element, '.HgListingDescription_description_r5HCO > :nth-child(1)')
+
+    desc = try_get_element_text(apartment_element, '.HgListingDescription_description_r5HCO > :nth-child(2)')
+
+    location = try_get_element_text(apartment_element, 'address')
+
+    price_text = try_get_element_text(apartment_element, '.HgListingCard_price_JoPAs')
+    price = None
+    if price is not None:
+        price = float(re.search(r'\d+', price_text).group())
+
+    rooms = try_get_element_text(apartment_element, '.HgListingRoomsLivingSpace_roomsLivingSpace_GyVgq > :nth-child(1) > strong')
+
+    surface_text = try_get_element_text(apartment_element, '.HgListingRoomsLivingSpace_roomsLivingSpace_GyVgq > :nth-child(2) > strong')
+    surface = None
+    if surface_text is not None:
+        surface = re.search(r'\d+', surface_text).group()
+
+    return {
+            'name': name,
+            'price': price,
+            'location': location,
+            'rooms': rooms,
+            'surface': surface,
+            'desc': desc
+        }
+
+def full_scrape_apartment(driver):
     name=None
     desc=None
     location=None
     price=None
     rooms=None
     surface=None
+    latitude=None
+    longitude=None
+    floor=None
     try:
         name = apartment_element.find_element(By.CSS_SELECTOR, '.HgListingDescription_description_r5HCO > :nth-child(1)').text
     except:
@@ -102,9 +147,6 @@ def scrape_apartment (apartment_element):
     try:
         price_text = apartment_element.find_element(By.CSS_SELECTOR, '.HgListingCard_price_JoPAs').text
         price = float(re.search(r'\d+', price_text).group())
-        by_month = bool(re.search(r'\bmonth\b', price_text, re.IGNORECASE))
-        if not by_month:
-            price /= 12
     except:
         pass
     try:
@@ -124,17 +166,55 @@ def scrape_apartment (apartment_element):
             'desc': desc
         }
 
-
-def scrape():
+def full_scrape():
     apartments = []
     
 
     # Regex para sacar nº total de apartamentos
     print(driver.find_element(By.CSS_SELECTOR, ".searchButton .HgButton_content_RMjt_").text)
     total = int(re.search(r'\d+', driver.find_element(By.CSS_SELECTOR, ".searchButton .HgButton_content_RMjt_").text).group())
-    idx = 0
     
-    start_time = time.time()
+    progress_bar = Progress_bar(total = total)
+    while True:
+
+        apartments = driver.find_elements(By.CSS_SELECTOR, 'div[role=listitem] a')
+
+        # Select the next page button if it exists
+        next_button = None
+        try:
+            next_button = driver.find_element(By.CSS_SELECTOR, 'a[aria-label="Go to next page"]')
+        except Exception as e:
+            next_button = None
+            
+        for apartment in apartments:
+
+            link = urljoin(apartment.get_attribute('href'), URL)
+
+            driver.get("")
+
+            apartments.append(full_scrape_apartment(driver))
+            
+            progress_bar.increment()
+            
+
+        # Cycle to next page if there is
+        if next_button is None:
+            break
+        time.sleep(1)
+        next_button.click()
+    print()
+    return apartments
+
+def fast_scrape():
+    apartments = []
+    
+
+    # Regex para sacar nº total de apartamentos
+    print(driver.find_element(By.CSS_SELECTOR, ".searchButton .HgButton_content_RMjt_").text)
+    total = int(re.search(r'\d+', driver.find_element(By.CSS_SELECTOR, ".searchButton .HgButton_content_RMjt_").text).group())
+
+    progress_bar = Progress_bar(total = total)
+
     while True:
 
         apartment_elements = driver.find_elements(By.CSS_SELECTOR, 'div[role=listitem]')
@@ -150,19 +230,7 @@ def scrape():
             
             apartments.append(scrape_apartment(apartment_element))
             
-
-            # Progress bar
-            idx += 1
-            end_time = time.time()
-            elapsed_time = end_time - start_time
-            mean_time = elapsed_time / idx
-            estimated_time = mean_time * (total-idx)
-            hours, remainder = divmod(estimated_time, 3600)
-            minutes, seconds = divmod(remainder, 60)
-            progress = int((idx) / total * BAR_WIDTH)
-            bar = '[' + '#' * progress + ' ' * (BAR_WIDTH - progress) + ']'
-            elapsed_time_formatted = "{:02}:{:02}:{:02}".format(int(hours), int(minutes), int(seconds))
-            print(f'\r{bar} {idx}/{total} apartments screaped {elapsed_time_formatted}', end='', flush=True)
+            progress_bar.increment()
 
         # Cycle to next page if there is
         if next_button is None:
@@ -201,7 +269,7 @@ try:
     search()
 
     # Extract data
-    apartments = scrape()
+    apartments = fast_scrape()
 
 except Exception as e:
     print()
